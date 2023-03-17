089413f42020e4d5e5311ed6d1e06db64e8a4742
Alessandro Saggese <asaggese@gmail.com>
2023-03-17 21:32:18 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // code to handle login
+  };
+
+  return (
+    <div className="login-box">
+      <h2>Login Here</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="user-box">
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+          <label>Username</label>
+        </div>
+        <div className="user-box">
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          <label>Password</label>
+        </div>
+        <button type="submit">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
